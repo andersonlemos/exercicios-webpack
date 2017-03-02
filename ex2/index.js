47fc82dd1de637de130b3363d81b2be967a90956
andersonlemos <anderson.lemos@dmorj.com.br>
2017-03-02 17:53:51 -0300

    ES6 transpilling , config babel-plugins

---
+++ ex2/index.js
@@ -0,0 +1,1 @@
+const duvida =  require('./duvidaCruel');
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,24 @@
-const webpack = require('webpack')
+const webpack = require('webpack');
 
 module.exports = {
     entry: './ex/index.js',
     output:{
-        path: __dirname + '/public',
-        filename: './bundle.js'
+            path: __dirname + '/public',
+            filename: './bundle.js'
     },
     devServer: {
-        port: 8081,
-        contentBase: './public'
+            port: 8081,
+            contentBase: './public'
+    },
+    module:{
+                loaders:[{
+                        test: /.js?$/,
+                        loader: 'babel-loader',
+                        exclude : /node_modules/,
+                        query:{
+                                presets:['es2015'],
+                                plugins:['transform-object-rest-spread']
+                        }
+                 }]
     }
 }
